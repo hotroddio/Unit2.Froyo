ca04cde71e3dabd5502ef5a507c01f5284e6ba0c
John Rodriguez <hotroddio@gmail.com>
2023-11-22 18:30:45 -0500

    Resubmittal Attempt

--- froyo.js
+++ froyo.js
@@ -1,24 +1,76 @@
 const userOrderString = prompt(
-    "Please enter your flavors here.",
-    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+    "Please enter your flavors here separated by comma's.",
+    "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
 );
-const stringArray = userOrderString.split(",");
-const flavors = [];
-for (let i = 0; i < stringArray.length; i++) {
-  const str = stringArray[i];
-  const flavor = parseInt(str);
-  flavors.push(flavor);
-}
-console.log('You have ordered the flavors ${fullOrder(flavors)}.');
+const stringArray = userOrderString.split(", ");
+console.log(stringArray);
 
-/** 
- * @param {flavor[]} numbers an array of integers
- * @returns {flavor} the length of the array
- */
-function fullOrder(order) {
-    let total = 0; 
-    for (const flavors in order) {
-        total += order[flavors]
+// let flavors = 0;
+// for (let i=0; i<stringArray.length; i++) {
+//     if (stringArray[i].status === '0') flavors++;
+// }
+// console.log(flavors);
+
+// function flavorCount() {
+//     let count = stringArray;
+//     console.log((r.match(stringArray)).length);
+// }
+
+let flavorTotal = {};
+for (let str of stringArray) {
+        if (flavorTotal[str]) {
+            flavorTotal[str]++;
+            }else {
+                flavorTotal[str]=1;
     }
-    return total;
-};
+}
+console.log(flavorTotal);
+
+alert(JSON.stringify(flavorTotal));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const stringArray = userOrderString.split(",");
+// const flavors = [];
+// for (let i = 0; i < stringArray.length; i++) {
+//   const str = stringArray[i];
+//   const flavor = parseInt(str);
+//   flavors.push(flavor);
+// }
+// console.log(`You have ordered the flavors ${fullOrder(flavors)}.`);
+
+// /** 
+//  * @param {flavor[]} numbers an array of integers
+//  * @returns {flavor} the length of the array
+//  */
+
+// function fullOrder(order) {
+//     let total = 0; 
+//     for (const flavors in order) {
+//         total += order[flavors]
+//     }
+//     return total;
+// };
+// console.log(total);
